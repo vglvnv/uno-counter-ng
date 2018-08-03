30e61c5c05c9e83893d8ee668f3993c5cb04895e
Vitalii Golovanev <v.golovanev@gmail.com>
2018-08-03 10:09:35 +0500

    Local storage service implementation and some effects draft

---
+++ src/app/local-storage.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import * as localForage from 'localforage';
+import { Observable, from } from 'rxjs';
+import { State } from './store/reducers/game.reducers';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+  setState(state: State): Observable<State> {
+    return from(localForage.setItem('gameState', state));
+  }
+  getState(): Observable<State> {
+    return from(localForage.getItem('gameState'));
+  }
+  clearState(): Observable<void> {
+    return from(localForage.removeItem('gameState'));
+  }
+  constructor() { }
+}
